efeb9d5a9c2d0fc2255438564689d9a76a012a00
Pedro Ceriotti <pedro@nekt.ai>
2025-01-31 16:34:15 -0300

    feat: Make small changes to adapt to Nekt standards

--- tests/test_core.py
+++ tests/test_core.py
@@ -4,33 +4,62 @@ from __future__ import annotations
 
 import typing as t
 
-import pytest
+from singer_sdk.helpers._compat import importlib_resources
 from singer_sdk.testing import get_target_test_class
+from singer_sdk.testing.suites import TestSuite
+from singer_sdk.testing.templates import TargetFileTestTemplate
 
+import tests.streams as test_streams
 from target_hubspot.target import TargetHubSpot
 
-# TODO: Initialize minimal target config
-SAMPLE_CONFIG: dict[str, t.Any] = {}
+if t.TYPE_CHECKING:
+    from singer_sdk.helpers._compat import Traversable
+
+SAMPLE_CONFIG: dict[str, t.Any] = {
+    "access_token": "",
+    "date_format": "YEAR_MONTH_DAY",
+    "import_operations": "UPSERT",
+    "column_mapping": [
+        {
+            "columnName": "email",
+            "propertyName": "email",
+            "columnObjectTypeId": "0-1",
+            "columnType": "HUBSPOT_ALTERNATE_ID",
+        },
+        {"columnName": "name", "propertyName": "firstname", "columnObjectTypeId": "0-1"},
+        {"columnName": "company_name", "propertyName": "company", "columnObjectTypeId": "0-1"},
+        {"columnName": "phone_number", "propertyName": "mobilephone", "columnObjectTypeId": "0-1"},
+    ],
+}
+
+
+class SampleData(TargetFileTestTemplate):
+    """Test Target record with date attributes."""
+
+    name = "sample_data"
+
+    @property
+    def singer_filepath(self) -> Traversable:
+        return importlib_resources.files(test_streams) / "sample_data.singer"
 
 
 # Run standard built-in target tests from the SDK:
-# StandardTargetTests = get_target_test_class(
-#     target_class=TargetHubSpot,
-#     config=SAMPLE_CONFIG,
-# )
+StandardTargetTests = get_target_test_class(
+    target_class=TargetHubSpot,
+    config=SAMPLE_CONFIG,
+    include_target_tests=False,
+    custom_suites=[
+        TestSuite(
+            kind="target",
+            tests=[
+                SampleData,
+            ],
+        )
+    ],
+)
 
 
-# class TestTargetHubSpot(StandardTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
-#     """Standard Target Tests."""
+class TestTargetHubSpot(StandardTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
+    """Standard Target Tests."""
 
-#     @pytest.fixture(scope="class")
-#     def resource(self):  # noqa: ANN201
-#         """Generic external resource.
-
-#         This fixture is useful for setup and teardown of external resources,
-#         such output folders, tables, buckets etc. for use during testing.
-
-#         Example usage can be found in the SDK samples test suite:
-#         https://github.com/meltano/sdk/tree/main/tests/samples
-#         """
-#         return "resource"
+    pass
